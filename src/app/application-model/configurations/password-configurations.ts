26ae993f731719c4e7c458f919f72b644032c17b
NaorSibony <Naorsibony@gmail.com>
2020-06-07 15:01:25 +0200

    version bump + explanation comment

--- src/app/application-model/configurations/password-configurations.ts
+++ src/app/application-model/configurations/password-configurations.ts
@@ -1,5 +1,7 @@
 import { PasswordValidatorsOptions } from '../validators/password-validators';
 
+// configure a min num of chars to force on the password on each validation. Please use ositive numbers only.
+// To remove a validation, simply remove it from the object and it'll be removed from the display/validation
 export const registrationPasswordConfiguration: PasswordValidatorsOptions = {
   minChars: 8,
   minLowerCase: 1,
